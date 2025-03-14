4b28baa1447e2a932e9abdb17149e1f2f9922faa
Bryan Maamo <ninbryan@users.noreply.github.com>
2025-03-14 12:10:31 -0700

    make coverage work and include to build yml

--- vitest.config.js
+++ vitest.config.js
@@ -1,32 +1,43 @@
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
-import { defineConfig } from 'vitest/config';
+import { coverageConfigDefaults, defineConfig } from 'vitest/config';
 import { storybookTest } from '@storybook/experimental-addon-test/vitest-plugin';
+import react from '@vitejs/plugin-react';
 
 const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
+const configDir = path.join(dirname, '.storybook');
+const setupFile = path.join(dirname, '.storybook/vitest.setup.js');
 
 // More info at: https://storybook.js.org/docs/writing-tests/test-addon
 export default defineConfig({
+  plugins: [
+    // because... it reactjs
+    react(),
+
+    // The plugin will run tests for the stories defined in your Storybook config
+    // See options at: https://storybook.js.org/docs/writing-tests/test-addon#storybooktest
+    storybookTest({ configDir, storybookScript: 'npx storybook --ci' })
+  ],
   test: {
-    workspace: [
-      {
-        extends: true,
-        plugins: [
-          // The plugin will run tests for the stories defined in your Storybook config
-          // See options at: https://storybook.js.org/docs/writing-tests/test-addon#storybooktest
-          storybookTest({ configDir: path.join(dirname, '.storybook') })
-        ],
-        test: {
-          name: 'storybook',
-          browser: {
-            enabled: true,
-            headless: true,
-            name: 'chromium',
-            provider: 'playwright'
-          },
-          setupFiles: ['.storybook/vitest.setup.js']
-        }
-      }
-    ]
+    name: 'storybook',
+    browser: {
+      enabled: true,
+      headless: true,
+      provider: 'playwright',
+      instances: [{ browser: 'chromium' }]
+    },
+    coverage: {
+      provider: 'istanbul',
+      exclude: [
+        ...coverageConfigDefaults.exclude,
+        '**/.storybook/**',
+        // 👇 This pattern must align with the `stories` property of your `.storybook/main` config
+        '**/*.stories.*',
+        // 👇 This pattern must align with the output directory of `storybook build`
+        '**/storybook-static/**',
+        '**/doc/**'
+      ]
+    },
+    setupFiles: [setupFile]
   }
 });
