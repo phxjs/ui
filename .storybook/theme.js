c210a3472b66f9bd04d3b49102480069fbb85c47
Bryan Maamo <ninbryan@users.noreply.github.com>
2025-03-14 08:36:38 -0700

    update logo link to github repo

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -3,9 +3,9 @@ import { create } from '@storybook/theming';
 export default create({
   base: 'light',
   brandTitle: '@phxjs/ui',
-  brandUrl: 'https://phx.js.org/ui',
+  brandUrl: 'https://github.com/phxjs/ui',
   brandImage: 'https://raw.githubusercontent.com/phxjs/.github/refs/heads/main/profile/logo.svg',
-  brandTarget: '_self',
+  brandTarget: '_blank',
 
   // Typography
   fontBase: '"Urbanist", sans-serif',
