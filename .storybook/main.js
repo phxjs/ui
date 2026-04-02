b017cf6ecdeef557c995dcd23d3ddd7b38993616
Bryan Maamo <ninbryan@users.noreply.github.com>
2026-04-01 22:32:03 -0700

    make it work based on ai for relative instead of globs

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,28 @@
+import { readdirSync } from 'node:fs';
+import path from 'node:path';
+
+const projectRoot = path.resolve(process.cwd());
+const files = '**/*.stories.@(jsx|js)';
+const titlePrefix = '@phxjs/ui';
+const DOT = '.';
+const ignoreDirectories = 'coverage|doc|node_modules';
+const directories = readdirSync(projectRoot, { withFileTypes: true });
+const stories = directories
+  .filter((entry) =>
+    entry.isDirectory() // be a directory
+    && !ignoreDirectories.includes(entry.name) // ignore certain  directories
+    && entry.name[0] !== DOT // no dot files/directories
+  )
+  .map((entry) => ({
+    directory: `../${entry.name}`, // for some reason... vitest wants relative path instead of absolute path...
+    files,
+    titlePrefix
+  }));
+
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
   framework: '@storybook/react-vite',
-  stories: [
-    {
-      directory: '../!(.*|coverage|doc|node_modules)', // match anything but pattern from https://github.com/micromatch/picomatch?tab=readme-ov-file#extglobs
-      files: '**/*.stories.@(jsx|js)',
-      titlePrefix: '@phxjs/ui', // 👈 Configure the title prefix
-    },
-  ],
+  stories,
   addons: [
     '@storybook/addon-a11y',
     '@storybook/addon-docs',
