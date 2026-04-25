b42cbdf82d8d0594bf448f952193f36f4f2f765e
Bryan Maamo <ninbryan@users.noreply.github.com>
2026-04-25 06:27:13 +0000

    delete comments

--- Popover/Popover.jsx
+++ Popover/Popover.jsx
@@ -20,9 +20,6 @@ export function Popover({
     console.error('pick one Popover option: manual or hint');
   }
 
-//https://kinu.sh/docs/popover
-// https://xandemon.github.io/developer-icons/icons/Design/
-//https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Global_attributes/popover#nesting_popovers
   return (
     <>
       {trigger(popoverTarget)}
